29debf1850af00c59a5616ea1845c0635fe0748d
Abirokaya <rokayaabinash9@gmail.com>
2025-05-28 10:59:34 +0545

    upload main in github

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ def index():
 
 
 if __name__=="__main__":
-    uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True,loop="asyncio")
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True,loop="asyncio")
 
